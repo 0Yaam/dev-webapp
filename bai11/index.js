8de1ee21cc4b39e15a04ae4b09843bf96469413a
TruongDan <nguyenngoctruongdan@gmail.com>
2026-04-21 12:15:42 +0700

    update source code

--- bai11/index.js
+++ bai11/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 9000;
+
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) =>{
--- bai14/bai1b.js
+++ bai14/bai1b.js
@@ -17,3 +17,4 @@ setTimeout(()=>{
 app.listen(port, ()=>{
     console.log(`server is running on ${port}`);
 });
+
